9e81f16d033140b535d9564411f8a743fac7d155
Blake Burbach <cras.bburbach@gmail.com>
2018-03-15 13:09:57 -0500

    got a request channel going.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,4 +1,21 @@
 $(document).ready(readyNow);
 function readyNow(){
     console.log('document ready');
+    $('#submitBtn').on('click', submitGame);
+}
+
+function submitGame(){
+    let gameName = $('#gameName').val();
+    let cost = $('#cost').val();
+    let gameToAdd = {name: gameName, cost: cost};
+    $.ajax({
+        type: 'POST',
+        data: gameToAdd, 
+        url: '/game'
+    }).done(function(response) {
+        //our response from POST will be '200' success
+        console.log('SUCCESS!!!');
+    }).fail(function(response) {
+        alert('Uh-oh! Something went wrong...');
+    })
 }
--- server/server.js
+++ server/server.js
@@ -1,9 +1,24 @@
 let express = require('express');
 let app = express();
 const PORT = 4001;
+let bodyParse = require('body-parser');
+const gameCollection = [];
+
+// configures body-parser for jQuery
+app.use(bodyParse.urlencoded({extended: true})); //<- will not work without this
 
 // Serve static files
 app.use(express.static('server/public'));
+
+app.post('/game', (req, res) => {
+    console.log(req.body);
+    let gameToAdd = req.body;
+    gameCollection.push(gameToAdd);
+    console.log(gameCollection);
+    res.sendStatus(200);
+});
+
+app.get('')
 
 // Spin up the server
 app.listen(PORT, () => {
