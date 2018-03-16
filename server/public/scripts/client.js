2f79abd0bf1f23d189a1f55ca573d2a6254fbbd8
Blake Burbach <cras.bburbach@gmail.com>
2018-03-16 14:23:05 -0500

    Hadar Game Face.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,6 +2,8 @@ $(document).ready(readyNow);
 function readyNow(){
     console.log('document ready');
     $('#submitBtn').on('click', submitGame);
+    getAllGames(); // call this on page load so that a user doesn't need to 
+                   // need to submit one game to get the list of all of the games
 }
 
 function submitGame(){
@@ -15,7 +17,36 @@ function submitGame(){
     }).done(function(response) {
         //our response from POST will be '200' success
         console.log('SUCCESS!!!');
+        // Refresh our game list
+        getAllGames();
     }).fail(function(response) {
         alert('Uh-oh! Something went wrong...');
     })
+}
+
+function getAllGames(){
+    $.ajax({
+        type: 'GET',
+        url: '/game'
+    }).done(function(response){
+        appendToDom(response); // response is our gameCollection
+    })
+}
+
+function appendToDom(gameCollection){
+    $('#gameContent').empty();
+    for (let game of gameCollection){
+        console.log('GAME: ', game);
+        let tr = $('<tr></tr>');
+        if ( game.isClearance){
+            tr.append('<td class="clearance">' + game.name + '</td>');
+            tr.append('<td class="clearance">' + game.cost + '</td>');
+            tr.append('<td class="clearance">' + game.tax + '</td>');
+        } else {
+            tr.append('<td>' + game.name + '</td>');
+            tr.append('<td>' + game.cost + '</td>');
+            tr.append('<td>' + game.tax + '</td>');
+        }
+        $('#gameContent').append(tr);
+    }
 }
--- server/server.js
+++ server/server.js
@@ -1,24 +1,48 @@
 let express = require('express');
 let app = express();
-const PORT = 4001;
+// if on heroku, use the provided port || use 4001
+const PORT = process.env.PORT || 4001;
 let bodyParse = require('body-parser');
-const gameCollection = [];
+const gameCollection = []; // Contains objects w/ {name: 'game name' ,cost: 0.99}
 
 // configures body-parser for jQuery
+// happens first
+//MUST BE DONE BEFORE app.post!!!
 app.use(bodyParse.urlencoded({extended: true})); //<- will not work without this
 
 // Serve static files
 app.use(express.static('server/public'));
 
+// when we want to add a new game.
 app.post('/game', (req, res) => {
     console.log(req.body);
     let gameToAdd = req.body;
+    let gameName = gameToAdd.name;
+    let gameCost = parseFloat(gameToAdd.cost);
+    gameToAdd.name += '!!!';
+     gameToAdd.tax = gameCost * 0.07;
+    // distinquish between 19.99 and 19.00
+    gameToAdd.isClearance = isClearance(gameCost);
+    // 19.99 - Math.floor(19.99); // 19.99 - 19 = 0.99
     gameCollection.push(gameToAdd);
     console.log(gameCollection);
     res.sendStatus(200);
-});
 
-app.get('')
+}); 
+//distinguish between .99 and .00
+function isClearance(cost){
+    if (cost - Math.floor(cost)=== 0){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+// Send back all the games.
+app.get('/game', (req, res) => {
+    res.send(gameCollection);
+})
 
 // Spin up the server
 app.listen(PORT, () => {
